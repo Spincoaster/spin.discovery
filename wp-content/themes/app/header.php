f61ccb2a613dfdf9a9c2da61a551f5743d3e1317
Hiroki Kumamoto <kumabook@live.jp>
2018-05-21 14:29:55 +0900

    Remove a link to lineup page temporarily

--- wp-content/themes/app/header.php
+++ wp-content/themes/app/header.php
@@ -32,7 +32,7 @@
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav navbar-right mr-auto">
               <li class="nav-item"><a class="nav-link" href="/">HOME</a></li>
-              <li class="nav-item"><a class="nav-link" href="/events/<?= $event ?>" >LINEUP</a></li>
+<!-- TODO           <li class="nav-item"><a class="nav-link" href="/events/<?= $event ?>" >LINEUP</a></li> -->
               <li class="nav-item"><a class="nav-link" href="/<?= $event ?>/timetable" >TIMETABLE</a></li>
               <li class="nav-item"><a class="nav-link" href="/<?= $event ?>/ticket" >TICKET</a></li>
               <li class="nav-item"><a class="nav-link" href="/<?= $event ?>/access" >ACCESS</a></li>
