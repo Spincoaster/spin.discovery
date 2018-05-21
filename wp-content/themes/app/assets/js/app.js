edd86ad5a8f749881c0a318d6ac06b73da6297ad
Hiroki Kumamoto <kumabook@live.jp>
2018-05-21 19:57:13 +0900

    Set height of `.header-image` and `.background` progmatically because calc(100vh) of iOS devices  varies depending on address bar visibility

---
+++ wp-content/themes/app/assets/js/app.js
@@ -0,0 +1,39 @@
+jQuery(function() {
+  var $ = jQuery;
+  var imageWidth = 1005;
+  var imageHeight =  1348;
+
+  function setWindowHeight() {
+    var height = $(window).height();
+    var width = $(window).width();
+
+    var w, h;
+    if (imageWidth / imageHeight > width / height) {
+      h = height;
+      w = height * imageWidth / imageHeight;
+    } else {
+      w = width;
+      h = width * imageHeight / imageWidth;
+    }
+
+    $('.header-container').css({
+      width: width,
+      height: height
+    });
+    $('.background').css({
+      backgroundSize: w + 'px ' + h + 'px'
+    });
+  }
+
+  function isIOS() {
+    var ua = navigator.userAgent;
+    return ua.indexOf('iPhone') >= 0
+      || ua.indexOf('iPad') >= 0
+      || navigator.userAgent.indexOf('iPod') >= 0;
+  }
+
+  $(window).on('orientationchange', setWindowHeight);
+  if (isIOS()) {
+    setWindowHeight();
+  }
+});
