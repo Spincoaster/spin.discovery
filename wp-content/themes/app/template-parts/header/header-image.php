2b30b96c7017ad4edbedfabfad741505effeafd2
Hiroki Kumamoto <kumabook@live.jp>
2018-05-21 14:26:52 +0900

    Change style of header-image caption. - align right - bold font - change size depending on device width

--- wp-content/themes/app/template-parts/header/header-image.php
+++ wp-content/themes/app/template-parts/header/header-image.php
@@ -2,8 +2,7 @@
   <div class="header-image-container col-10 offset-1 col-lg-6 offset-lg-3 col-sm-8 offset-sm-2">
     <img class="header-image" src="<?= get_template_directory_uri(); ?>/assets/images/logo.png">
     <div class="header-image-caption">
-      <p>VOL. 07</p>
-      <p>2018.06.10 (SUN) WALL & WALL</p>
+     <p>2018.06.10 SUN<br>WALL & WALL</p>
     </div>
   </div>
 </div>
