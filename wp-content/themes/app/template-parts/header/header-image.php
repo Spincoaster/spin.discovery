1ba9f6f6a9bb781dbfd6fba10f8fdb4ff82de0d9
Hiroki Kumamoto <kumabook@live.jp>
2018-05-21 13:52:29 +0900

    Change header-image size dynamically depending of device width

--- wp-content/themes/app/template-parts/header/header-image.php
+++ wp-content/themes/app/template-parts/header/header-image.php
@@ -1,5 +1,5 @@
 <div>
-  <div class="header-image-container col-lg-10 offset-lg-1 col-sm-12">
+  <div class="header-image-container col-10 offset-1 col-lg-6 offset-lg-3 col-sm-8 offset-sm-2">
     <img class="header-image" src="<?= get_template_directory_uri(); ?>/assets/images/logo.png">
     <div class="header-image-caption">
       <p>VOL. 07</p>
