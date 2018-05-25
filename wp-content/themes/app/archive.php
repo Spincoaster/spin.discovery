21850e07aa7c993c8e6668d69de6c4fa73406278
Hiroki Kumamoto <kumabook@live.jp>
2018-05-25 21:58:09 +0900

    Implement archive.php for event as lineup page: image, title, content, youtube. TODO: social links

--- wp-content/themes/app/archive.php
+++ wp-content/themes/app/archive.php
@@ -8,10 +8,38 @@ if ( is_tax( 'events' ) ) {
     while ( have_posts() ) :
       the_post();
       $title = get_the_title();
-      $custom_fields = get_post_meta(get_the_id(), 'twitter', true);
-      echo '<h2 class="lineup-artist-title">'.$title.'</h2>';
+      $content = get_the_content();
+      $twitter = get_post_meta(get_the_id(), 'twitter', true);
+      $youtube = get_post_meta(get_the_id(), 'youtube', true);
       $image_id = get_post_meta(get_the_id(), 'image', true);
+
+      echo '<div class="row">';
+
+      echo '  <div class="col-12">';
+      echo '  <h2 class="lineup-artist-title">'.$title.'</h2>';
+      echo '</div>';
+
+      echo '<div class="col-6">';
       echo wp_get_attachment_image($image_id, 'large', false, array( 'class' => 'lineup-artist-image' ));
+      echo '</div>';
+
+      echo '<div class="col-6">';
+      echo '  <div class="row">';
+      echo '    <div class="col-12">';
+      echo '      <p>'.$content.'</p>';
+      echo '    </div>';
+
+      echo '    <div class="col-12">';
+      echo '      <iframe src="'.$youtube.'" allow="autoplay; encrypted-media" allowfullscreen="" width="500" height="281" frameborder="0"></iframe>';
+      echo '    </div>';
+
+      echo '    <div class="col-12">';
+      echo '      <p>social links</p>';
+      echo '    </div>';
+      echo '  </div>';
+      echo '</div>';
+
+      echo '</div>';
   ?>
 
 <?php
