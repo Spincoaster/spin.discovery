a44c006c4b0ae8b78e693386aa700c2c05eab506
Hiroki Kumamoto <kumabook@live.jp>
2021-07-02 22:48:03 +0900

    Add sponsor and app link to footer

--- wp-content/themes/app/footer.php
+++ wp-content/themes/app/footer.php
@@ -1,3 +1,31 @@
+<p><h3><div style="text-align:center;"> SPONSORS</div></h3></p>
+
+<div style="text-align:center;">
+  <span>
+    <a href="https://www.nex-tone.co.jp/" rel="noopener noreferrer" target="_blank"><img src="https://d1rm4mnq8j8biv.cloudfront.net/wp-content/uploads/2021/06/25181311/NexTone_logo_White_horizontal-300x72.png" alt="" width="150"  class="aligncenter size-medium wp-image-1248" /></a><a href="https://artist-led.com/" rel="noopener noreferrer" target="_blank"><img src="https://d1rm4mnq8j8biv.cloudfront.net/wp-content/uploads/2021/06/25181420/%E3%82%A2%E3%82%BB%E3%83%83%E3%83%88-11-300x85.png" alt="" width="150" class="aligncenter size-medium wp-image-1249" /></a>
+  </span>
+</div>
+<small><div style="text-align:center;">ArtLedは、アーティストの意思を尊重し自由な活動をサポートする、<br>
+新しい時代のデジタルマーケティングブランドです<br>
+ArtLed is powered by NexTone Inc.<br>
+<a href="https://artist-led.com/" rel="noopener noreferrer" target="_blank">https://artist-led.com/</a></span> </div></small>
+<br>  
+
+<div style="text-align:center;"> <span><a href="http://www.fujipacific.co.jp//" rel="noopener noreferrer" target="_blank"><img src="https://d1rm4mnq8j8biv.cloudfront.net/wp-content/uploads/2021/07/01152728/fpm_logo_en.jpg" alt="" width="300"  class="aligncenter size-medium wp-image-1248" /></a></span> </div>
+<br>  
+<br>  
+
+<div class="row justify-content-center d-none d-lg-flex d-xl-flex" style="text-align: center;">
+  <a href="https://apps.apple.com/us/app/spin-discovery/id1573252673?itsct=apps_box_badge&amp;itscg=30200" style="display: inline-block; overflow: hidden; border-radius: 13px; width: 250px; height: 83px;"><img src="https://tools.applemediaservices.com/api/badges/download-on-the-app-store/black/ja-jp?size=250x83&amp;releaseDate=1625097600&h=72f2e794202ca0d890641960bab4c17d" alt="Download on the App Store" style="border-radius: 13px; width: 250px; height: 83px;"></a><a href='https://play.google.com/store/apps/details?id=com.spincoaster.spin_discovery&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='Google Play で手に入れよう' src='https://play.google.com/intl/en_us/badges/static/images/badges/ja_badge_web_generic.png' style="margin-top:-20px; margin-right: -74px; height: 124px;"/></a>
+</div>
+
+<div class="d-lg-none" style="text-align: center;">
+  <a href="https://apps.apple.com/us/app/spin-discovery/id1573252673?itsct=apps_box_badge&amp;itscg=30200" style="display: inline-block; overflow: hidden; border-radius: 13px; width: 250px; height: 83px;"><img src="https://tools.applemediaservices.com/api/badges/download-on-the-app-store/black/ja-jp?size=250x83&amp;releaseDate=1625097600&h=72f2e794202ca0d890641960bab4c17d" alt="Download on the App Store" style="border-radius: 13px; width: 250px; height: 83px;"></a>
+</div>
+<div class="d-lg-none" style="text-align: center;">
+  <a href='https://play.google.com/store/apps/details?id=com.spincoaster.spin_discovery&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='Google Play で手に入れよう' src='https://play.google.com/intl/en_us/badges/static/images/badges/ja_badge_web_generic.png' style="height: 124px;"/></a>
+</div>
+
   <footer id="footer">
    <div class="site_footer">
      <div class="col-lg-12 col-md-12 col-sm-12">
