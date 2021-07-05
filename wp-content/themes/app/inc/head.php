1e0f817ff21c1058b29c0f5c9180ad29511a433c
Hiroki Kumamoto <kumabook@live.jp>
2021-07-05 13:32:29 +0900

    Use app theme version query for cache bustling

--- wp-content/themes/app/inc/head.php
+++ wp-content/themes/app/inc/head.php
@@ -1,11 +1,12 @@
 <?php
 
 function app_scripts() {
+    $app_theme = wp_get_theme();
     wp_enqueue_style( 'font-oswald', '//fonts.googleapis.com/css?family=Oswald:400,700,300');
     wp_enqueue_style( 'font-roboto', '//fonts.googleapis.com/css?family=Roboto:400,400italic,700');
     wp_enqueue_style( 'fontawesome', '//use.fontawesome.com/releases/v5.0.6/css/all.css');
     wp_enqueue_style( 'bootstrap-style', '//stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css');
-    wp_enqueue_style( 'app-style', get_stylesheet_uri(), array('bootstrap-style') );
+    wp_enqueue_style( 'app-style', get_stylesheet_uri(), array('bootstrap-style'), $app_theme->get( 'Version' ) );
     wp_enqueue_script( 'jquery', '//code.jquery.com/jquery-3.3.1.slim.min.js');
     wp_enqueue_script( 'popper', '//cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js');
     wp_enqueue_script(
